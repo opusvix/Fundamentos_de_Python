51c89203052af70d62d23abe6fa2a0ac1dd3b521
romulovieira777 <romulo.vieira777@gmail.com>
2021-12-04 11:05:34 -0300

    Exercícios de Fixação

---
+++ Seção 02 - Exercícios de Fixação/01 - Exercício.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name: ").strip().title()
+last_name = input("Enter your last name: ").strip().title()
+
+print(f"{first_name} {last_name}")
---
+++ Seção 02 - Exercícios de Fixação/02 - Exercício.py
@@ -0,0 +1,5 @@
+number_01 = int(input("Enter your first number: "))
+number_02 = int(input("Enter your second number: "))
+number_03 = int(input("Enter your third number: "))
+
+print(f'{number_01} + {number_02} + {number_03} = {number_01 + number_02 + number_03}')
